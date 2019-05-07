c4b3bcd5a3bca82303e27a2c67d7f8f071967241
zhangrunqing <zhangrunqing2009@126.com>
2019-05-07 13:14:35 -0700

    Initial Commit

---
+++ masonryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  masonryTest
+//
+//  Created by zrq on 2019/5/7.
+//  Copyright © 2019 zrq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ masonryTestTests/masonryTestTests.m
@@ -0,0 +1,39 @@
+//
+//  masonryTestTests.m
+//  masonryTestTests
+//
+//  Created by zrq on 2019/5/7.
+//  Copyright © 2019 zrq. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface masonryTestTests : XCTestCase
+
+@end
+
+@implementation masonryTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
